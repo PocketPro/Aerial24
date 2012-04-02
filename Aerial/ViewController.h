4a72c936a81c9c7edf7c4063eca1d3104da14a63
Gord Parke <grparke@gmail.com>
2012-04-01 20:55:54 -0400

    Initial Commit

---
+++ Aerial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Aerial
+//
+//  Created by Gord Parke on 4/1/12.
+//  Copyright (c) 2012 PPG Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Aerial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Aerial
+//
+//  Created by Gord Parke on 4/1/12.
+//  Copyright (c) 2012 PPG Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
